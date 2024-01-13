a9918ecc9456366e0ae9396e940ed482a083b91f
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-01-13 15:02:41 -0600

    Added double jump

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var jumpCount = 0
 @onready var mainChar = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -16,15 +17,21 @@ func _physics_process(delta):
 	else:
 		mainChar.animation = "default"
 	
-	
 	# Not on ground? Add gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		mainChar.animation = "jumping"
+	
+	# Touch ground
+	if is_on_floor():
+		jumpCount = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if jumpCount < 2:
+			velocity.y = JUMP_VELOCITY
+			jumpCount = jumpCount + 1
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
