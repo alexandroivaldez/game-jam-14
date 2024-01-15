96bd2c89afb9e1411c91b7d23bf609fca5ab36e0
Caio Takeshi <51913616+shimworks@users.noreply.github.com>
2024-01-15 16:42:40 -0300

    Added breakable Blocks
    Breakable blocks spawn coins on Hit
    If has no HP Left next hit break the blocks spawning pieces.
    
    Blocks HP can be adjusted individualy in propriets
    
    Added new Level called "level-tester" for easier mecanics testing

---
+++ scripts/break_box.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const box_pieces = preload("res://prefabs/box_pieces.tscn")
+const coin_instance = preload("res://prefabs/rigid_coin.tscn")
+
+@onready var animation_player := $anim as AnimationPlayer
+@onready var spawn_coin := $spawn_coin as Marker2D
+@export var pieces : PackedStringArray
+@export var hitpoints := 3
+var impulse := 200
+
+func break_sprite():
+	for piece in pieces.size():
+		var piece_instance = box_pieces.instantiate()
+		get_parent().add_child(piece_instance)
+		piece_instance.get_node("texture").texture = load(pieces[piece])
+		piece_instance.global_position = global_position
+		piece_instance.apply_impulse(Vector2(randi_range(-impulse, impulse), randi_range(-impulse, -impulse * 2)))
+	queue_free()
+
+func create_coin():
+	var coin = coin_instance.instantiate()
+	get_parent().call_deferred("add_child", coin)
+	coin.global_position = spawn_coin.global_position
+	coin.apply_impulse(Vector2(randi_range(-150,150), -450))
