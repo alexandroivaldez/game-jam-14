7b97c78a1e19b6b3186857488aa5248ba85827a5
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-01-19 21:44:27 -0600

    Added game start sequence

---
+++ scripts/water_pipe.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@onready var water_pipe_animation = $AnimatedSprite2D
+@onready var gem_game_start = $"../gem_game-start"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_gem_gamestart_body_entered(body):
+	water_pipe_animation.animation = "water_flow"
+	gem_game_start.queue_free()
+	await get_tree().create_timer(5.0).timeout
+	$"../seaTest".seasUp()
+	
