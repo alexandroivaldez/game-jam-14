3a4ec1b8fffdb1db5cbfd5fb9b73e385b7b558d1
Caio Takeshi <51913616+shimworks@users.noreply.github.com>
2024-01-25 17:50:08 -0300

    Added pause menu
    - Resume game
    - Retry
    - Quit

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+@onready var resume_btn = $menu_holder/resume_btn
+
+func _ready():
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume_btn.grab_focus()
+
+func _on_resume_btn_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_retry_btn_pressed():
+	get_tree().paused = false
+	Globals.coins = 0
+	Globals.score = 0
+	Globals.player_life = 3
+	await get_tree().create_timer(0.25).timeout
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
