ce1cd1ad5c641f87bfd87d934964c7112b9ad166
Alexandro Valdez <alexandroivaldez@gmail.com>
2024-01-25 20:39:06 -0600

    Fixed a small tileMap  issue

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -95,6 +95,7 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		await get_tree().create_timer(.30).timeout
 		queue_free()
 		emit_signal("player_has_died")
+		
 	if knockback_force != Vector2.ZERO:
 		knockback_vector = knockback_force
 		var knockback_tween := get_tree().create_tween()
