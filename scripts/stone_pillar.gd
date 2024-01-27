1b41b8b73681ca002a792eeac4d8dc48231d7abd
Caio Takeshi <51913616+shimworks@users.noreply.github.com>
2024-01-26 22:16:59 -0300

    Added sprites

--- scripts/stone_pillar.gd
+++ scripts/stone_pillar.gd
@@ -1,7 +1,7 @@
 extends AnimatableBody2D
 
 var movePillar = false
-var speed = 50
+var speed = 100
 var pillarCount = 0
 
 func _ready():
@@ -10,7 +10,7 @@ func _ready():
 func _process(delta):
 	if (movePillar):
 		# The perfect num to lift and stop the pillar right before it's sprite ends.
-		if (pillarCount < 245):
+		if (pillarCount < 300):
 			position.y -= speed * delta
 			pillarCount = pillarCount + 1
 		else:
